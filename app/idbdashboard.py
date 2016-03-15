842b6cc5c5fc1e5dd6b1412284250edc662f64e7
Taavo <ipengtaoxx@gmail.com>
2016-03-15 22:25:15 +0800

    fix

--- app/idbdashboard.py
+++ app/idbdashboard.py
@@ -6,13 +6,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from urllib import request
 from datetime import datetime
-import  time
+import time
 import hashlib
-import base64
-from Crypto import Random
-from Crypto.Cipher import AES
+import os, sched
 
-import pyaes
 
 app = Flask(__name__)
 db = SQLAlchemy()
@@ -26,44 +23,28 @@ db = SQLAlchemy()
 # print (request.urlopen('https://www.douban.com/photos/album/1623990634/').read().decode('utf-8'))
 
 
-# time = int(time.time()) * 1000
-#
-#
-# password = '123:'+ str(time)# print('没有加密的密码'+password)
-# md5 = hashlib.md5('123:1458032950377'.encode("utf-8"))
-# encryptPassword = md5.hexdigest()
-#
-# print('加密之后的密码'+md5.hexdigest())
-# print(time)
-# # print (time.time())
-# # NSString *encryptPassword = [[NSString stringWithFormat:@"%@:%lld", password, time] md5];
-#
+def refresh():
+    with request.urlopen('https://www.douban.com/photos/album/1623990634/') as f:
+        data = f.read();
+        print('Status:', f.status, f.reason)
 
 
-enc = 'Xu/FnoCtKClwIaJXWOgR9643PR3zMsGBDYuOung095c/2hZXgNVtwBY4RBiUlY0gbMfvs1DhignWEq+ayZYSQC7NzlQ9PaaYtegDqjadohGj1/N/TUhA5nH6zCchdo7A+GFsokA8pQKQIM7sH4SKHkqfkZAAAFQ7+Y3ZJp3xe+tGM132d6O3Q6r3S748nWfgbSDQC9Y/nQJI0tne1HRJ/XFaZpO3npNCf2shphXR06K31NgHVC0bnTdVNRGqTUBJbyvMGqOjKQgoBD6J5to/zusP3CnQX0ZjriawkQwOs9gURsMV8Gpmed9Zku/mkNV8R9P+C5CE3K0ue8v13mw65B2cLk/fJQ4ZoMMmVPQe37Yg8aoLBPuGqghtzFf/TbWQxPqjNNGp+qph9WKXiAm7xrNXMOCPkD9VlVN+lNidzDlJ0RWW95tLV+bQSiyk2j4cZzannlS3RKlxke/OVXJImpJxTuvC+X+YdTUu6G2Rneoy5C9o7dzifx7Gy6rgGpALiFb6ePltZWrZsdvktP2w2A8sl54Btjc+6LdCfj4kH+pd80DmIzCCwpMXe+AV/O7TyZTpJIDiCfT06D6nr5g0DPi6WE3rBGqo524zq9DLLESxQvP5oowpi43mZfM4wZvdhKFO9vQ7ZRiNSLmxsDhTXek7ZhjaIJrGFmXHMHBTBODlkxv4cPUzXhZWtDChQlqX2azIJyQiQMPebIao3Ltb63A3VWAirqGR3a2k/B4tXEmiDCID3PihgUXI3ZA3ONCtN1STzoIQu5gjjaIgxPS0n7w1FGWOcF3vTvnvQSG+ksle1X/YPCHidDoFgPo3L9XdB2xTAwxMGmsQLUUj8oJWC7dEuaK2+sZA3IvUvo4oN5ZJkArAIqCi2VOuIH/Yo5TGpND/JEFqpyB/KGzeJOe1GajW6MIXjcAdn91M8TAFPSvwJML9Cd2FTCqZocWbMDCV7SxHPqKplIjNPMIoV8BRQm4ApzojxeAzzzW9LMOfNGvcJwFJC/JLOzjqcweiNqiLG71jfyHNdOWmwZ57sW81zgxBK+jh0ZTeC+O7Bg5XS1s='
-# iv = enc[:AES.block_size]
-# cipher = AES.new('ihlih*0037JOHT*)(PIJY*(()JI^)IO%', AES.MODE_CBC, iv)
-# one = cipher.decrypt(enc[AES.block_size:])
-# two = one[:-ord(one[len(one)-1:])]
-# three = two.decode()
-#
-# AESdecrypt(enc,'ihlih*0037JOHT*)(PIJY*(()JI^)IO%')
 
 
-key = 'ihlih*0037JOHT*)(PIJY*(()JI^)IO%'
+s = sched.scheduler(time.time, time.sleep)
+def do_something(sc):
+    refresh()
+    # do your stuff
+    sc.enter(5, 1, do_something, (sc,))
+
+s.enter(0, 1, do_something, (s,))
+# s.run()
 
 
-aes = pyaes.AESModeOfOperationCBC(bytes(key, encoding = "utf8")  )
-print(base64.b64decode(enc))
-
-# decrypted = aes.decrypt(base64.b64decode(enc))
-# print(decrypted)
-# print(decrypted.decode(encoding='gb2312'))
-# print(str(decrypted, encoding = "gbk")  )
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    addData()
+    # addData()
     print('There is none root.')
     return render_template("index.html",message='我的第一个jinja程序')
     return 'Hello World!'
