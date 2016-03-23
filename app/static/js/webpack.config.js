a419aac910ebb833b3772fa2c094d9be57dd8835
tttpeng <ipengtaoxx@gmail.com>
2016-03-23 18:15:34 +0800

    es6 delete

--- app/static/js/webpack.config.js
+++ app/static/js/webpack.config.js
@@ -19,7 +19,6 @@ module.exports = {
     },
     plugins: [
         new webpack.ProvidePlugin({
-            'Promise': 'es6-promise',
             'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
         })
     ],
