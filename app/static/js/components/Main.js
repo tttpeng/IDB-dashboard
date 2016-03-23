c40674e3df90e3d474838d20055628de91f582c2
tttpeng <ipengtaoxx@gmail.com>
2016-03-23 12:00:37 +0800

    优化bundle大小

--- app/static/js/components/Main.js
+++ app/static/js/components/Main.js
@@ -14,12 +14,6 @@ import TableFooter from 'material-ui/lib/table/table-footer';
 import TextField from 'material-ui/lib/text-field';
 import Toggle from 'material-ui/lib/toggle';
 import Card from 'material-ui/lib/card/card';
-import CardActions from 'material-ui/lib/card/card-actions';
-import CardHeader from 'material-ui/lib/card/card-header';
-import CardMedia from 'material-ui/lib/card/card-media';
-import CardTitle from 'material-ui/lib/card/card-title';
-import FlatButton from 'material-ui/lib/flat-button';
-import CardText from 'material-ui/lib/card/card-text';
 
 const styles = {
   propContainerStyle: {
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,26 +1,10 @@
 var React = require("react");
-var MaterialUI = require("material-ui")
-//var MessageBoard = require("./components/MessageBoard");
-
-//import React from 'react';
+//var MaterialUI = require("material-ui")
 var ReactDOM = require("react-dom")
-//import ReactDOM from 'react-dom';
 var Main = require("./components/Main")
-var RaisedButton = require('material-ui/lib/raised-button');
 var injectTapEventPlugin = require("react-tap-event-plugin")
-//import Main from './Main'; // Our custom react component
 injectTapEventPlugin();
-//React.render(<MaterialUI.AppBar/>,document.getElementById("message-board-container"));
-//Main.js.render(<ProdcutGrid />, document.getElementById("message-board-container"));
-var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
-    );
-  }
-});
+
 ReactDOM.render(
   <Main />,
   document.getElementById('message-board-container')
